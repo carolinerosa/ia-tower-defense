ca50e21edc66a409b0542b5e165af9aeeed5cf4e
thyagotaian.nave <thyagotaian.nave@7c54a519-24e8-f7f1-1c71-ec4d1aed2465>
2013-12-03 23:59:34 +0000

    Projeto reorganizado em 2 pastas: uma para o projeto e outra para os modelos 3D.

---
+++ trunk/Projeto/TowerDefenseIA/TowerDefenseIA/Classes/SpawnManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TowerDefenseIA
+{
+    public class SpawnManager : DrawableGameComponent
+    {
+        Model model;
+        Grid grid;
+
+        public static Queue<Enemy>[] enemyQueues = new Queue<Enemy>[5];
+
+        public SpawnManager(Game game, Grid grid) : base(game)
+        {
+            this.grid = grid;
+
+            for (int i = 0; i < enemyQueues.Length; i++)
+            {
+                enemyQueues[i] = new Queue<Enemy>();
+            }
+
+            Game.Components.Add(this);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            
+            Vector3 enemyPosition = new Vector3(grid.GetPlane(0, 9).GetPosition().X, 1, grid.GetPlane(0, 9).GetPosition().Z); 
+
+            new Enemy(Game, Vector3.One, new Vector3(-90, 0, 90), enemyPosition, model, 0);
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            model = Game.Content.Load<Model>(@"Models\coco_bazoca");
+        }
+    }
+}
---
+++ trunk/Projeto/TowerDefenseIA/TowerDefenseIA/Game1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TowerDefenseIA
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            Window.Title = "Towe Defense - Mega Saldão de Torres";
+            IsMouseVisible = true;
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Input input = new Input(this);
+
+            Main main = new Main(this, spriteBatch);
+            Components.Add(main);
+
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
